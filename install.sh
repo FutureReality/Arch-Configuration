f02f79ce6281cfa309d81e79a22ad94f45b51317
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-04-11 19:23:29 +0200

    otra cosa

--- install.sh
+++ install.sh
@@ -1,96 +1,166 @@
 #!/bin/bash
 
-# Función para centrar el texto generado por Figlet
-centrar_figlet() {
-    local texto="$1"
-    local ancho_terminal=$(tput cols)
-    local texto_figlet=$(figlet "$texto")
-    local num_lineas=$(echo "$texto_figlet" | wc -l)
-    local max_chars=$(echo "$texto_figlet" | awk '{ if (length > max) {max = length}} END {print max}')
-    local espacios=$(( ($ancho_terminal - $max_chars) / 2 ))
-    
-    # Imprimir las líneas centradas
-    echo "$texto_figlet" | sed "s/^/$(printf '%*s' $espacios)/"
-}
+pacman -Sy figlet
 
-# Función para preguntar y instalar paquetes
-preguntar_instalar() {
-    local nombre_paquete="$1"
-    echo "¿Instalar $nombre_paquete? (y/n)"
-    read respuesta
-    if [ "$respuesta" = "y" ]; then
-        pacman -Sy --noconfirm "$nombre_paquete"
-    elif [ "$respuesta" = "n" ]; then
-        echo "Saltando al siguiente apartado..."
-    fi
-}
+figlet "Install All?"
+echo "[y/n]"
+read instalar_todo
+if [ "$instalar_todo" = "y" ]; then
+    pacman -Sy --noconfirm lightdm qtile picom nitrogen nano neovim git mc ranger firefox tor lynx neofetch htop glances net-tools iproute2 curl nmap p7zip gpg rsync duplicity jp2a cmatrix fail2ban iptables openssh snort mutt pass gnuchess frotz angband cataclysm-dda nethack lolcat tree
+    exit 0
+elif [ "$instalar_todo" = "n" ]; then
+    echo "Loading Stuff"
+fi
 
-# Presentación de los paquetes
-centrar_figlet "Instalación de paquetes"
-centrar_figlet "Entorno Gráfico"
+figlet "Entorno Gráfico"
 echo "lightdm qtile picom nitrogen"
-preguntar_instalar "Entorno Gráfico"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm lightdm qtile picom nitrogen
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Editor de Texto"
+figlet "Editor de Texto"
 echo "nano neovim"
-preguntar_instalar "Editor de Texto"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm nano neovim
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Administrador de Versiones"
+figlet "Administrador de Versiones"
 echo "git"
-preguntar_instalar "Administrador de Versiones"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm git
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Explorador de Archivos"
+figlet "Explorador de Archivos"
 echo "mc ranger"
-preguntar_instalar "Explorador de Archivos"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm mc ranger
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Navegadores"
+figlet "Navegadores"
 echo "firefox tor lynx"
-preguntar_instalar "Navegadores"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm firefox tor lynx
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Información del Sistema"
+figlet "Información del Sistema"
 echo "neofetch htop glances net-tools iproute2 curl"
-preguntar_instalar "Información del Sistema"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm neofetch htop glances net-tools iproute2 curl
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Escaneo de Red"
+figlet "Escaneo de Red"
 echo "nmap"
-preguntar_instalar "Escaneo de Red"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm nmap
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Comprimir Archivos"
+figlet "Comprimir Archivos"
 echo "p7zip"
-preguntar_instalar "Comprimir Archivos"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm p7zip
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Encriptación de Datos"
+figlet "Encriptación de Datos"
 echo "gpg"
-preguntar_instalar "Encriptación de Datos"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm gpg
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Copias de Seguridad"
+figlet "Copias de Seguridad"
 echo "rsync duplicity"
-preguntar_instalar "Copias de Seguridad"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm rsync duplicity
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Conversor de Imágenes a ASCII"
+figlet "Conversor de Imágenes a ASCII"
 echo "jp2a"
-preguntar_instalar "Conversor de Imágenes a ASCII"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm jp2a
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Salvapantallas"
+figlet "Salvapantallas"
 echo "cmatrix"
-preguntar_instalar "Salvapantallas"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm cmatrix
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Seguridad"
+figlet "Seguridad"
 echo "fail2ban iptables openssh snort"
-preguntar_instalar "Seguridad"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm fail2ban iptables openssh snort
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Correo Electrónico"
+figlet "Correo Electrónico"
 echo "mutt"
-preguntar_instalar "Correo Electrónico"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm mutt
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Gestor de Contraseñas"
+figlet "Gestor de Contraseñas"
 echo "pass"
-preguntar_instalar "Gestor de Contraseñas"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm pass
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Juegos"
+figlet "Juegos"
 echo "gnuchess frotz angband cataclysm-dda nethack"
-preguntar_instalar "Juegos"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm gnuchess frotz angband cataclysm-dda nethack
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
 
-centrar_figlet "Misceláneo"
+figlet "Misceláneo"
 echo "lolcat tree"
-preguntar_instalar "Misceláneo"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm lolcat tree
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
