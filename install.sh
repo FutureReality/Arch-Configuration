e418c2d6ec98bfe95254ba5e6058fb333b86a3f3
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-04-11 17:10:03 +0200

    aa

--- install.sh
+++ install.sh
@@ -29,4 +29,137 @@ elif [ "$respuesta" = "n" ]; then
     echo "Saltando al siguiente apartado..."
 fi
 
-# Continuar con el resto de las secciones de la misma manera...
+figlet "Administrador de Versiones"
+echo "git"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm git
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Explorador de Archivos"
+echo "mc ranger"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm mc ranger
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Navegadores"
+echo "firefox tor lynx"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm firefox tor lynx
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Información del Sistema"
+echo "neofetch htop glances net-tools iproute2 curl"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm neofetch htop glances net-tools iproute2 curl
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Escaneo de Red"
+echo "nmap"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm nmap
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Comprimir Archivos"
+echo "p7zip"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm p7zip
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Encriptación de Datos"
+echo "gpg"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm gpg
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Copias de Seguridad"
+echo "rsync duplicity"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm rsync duplicity
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Conversor de Imágenes a ASCII"
+echo "jp2a"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm jp2a
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Salvapantallas"
+echo "cmatrix"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm cmatrix
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Seguridad"
+echo "fail2ban iptables openssh snort"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm fail2ban iptables openssh snort
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Correo Electrónico"
+echo "mutt"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm mutt
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Gestor de Contraseñas"
+echo "pass"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm pass
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Juegos"
+echo "gnuchess frotz angband cataclysm-dda nethack"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm gnuchess frotz angband cataclysm-dda nethack
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+figlet "Misceláneo"
+echo "lolcat tree"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+    pacman -Sy --noconfirm lolcat tree
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
