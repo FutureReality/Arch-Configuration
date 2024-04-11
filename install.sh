3aef2342f171dd3981f4ae6766b1110b4e7e2623
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-04-11 14:24:22 +0200

    Añadida opcion de instalar todo
    Para que no se haga pesado

--- install.sh
+++ install.sh
@@ -4,11 +4,15 @@ sudo su
 
 pacman -Sy figlet
 
-echo "Bienvenido/a al instalador de paquetes. ¿Deseas instalar los siguientes paquetes? (y/n)"
+echo "Bienvenido/a al instalador de paquetes. ¿Deseas instalar todos los paquetes? (y/n)"
+read instalar_todo
+if [ "$instalar_todo" = "y" ]; then
+    pacman -Sy --noconfirm lightdm qtile picom nitrogen nano neovim git mc ranger firefox tor lynx neofetch htop glances net-tools iproute2 curl nmap p7zip gpg rsync duplicity jp2a cmatrix fail2ban iptables openssh snort mutt pass gnuchess frotz angband cataclysm-dda nethack lolcat tree
+    exit 0
+fi
 
 figlet "Entorno Gráfico"
 echo "lightdm qtile picom nitrogen"
-echo "y / n"
 read respuesta
 if [ "$respuesta" = "y" ]; then
     pacman -Sy --noconfirm lightdm qtile picom nitrogen
@@ -18,7 +22,6 @@ fi
 
 figlet "Editor de Texto"
 echo "nano neovim"
-echo "y / n"
 read respuesta
 if [ "$respuesta" = "y" ]; then
     pacman -Sy --noconfirm nano neovim
@@ -26,152 +29,4 @@ elif [ "$respuesta" = "n" ]; then
     echo "Saltando al siguiente apartado..."
 fi
 
-figlet "Administrador de Versiones"
-echo "git"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm git
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Explorador de Archivos"
-echo "mc ranger"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm mc ranger
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Navegadores"
-echo "firefox tor lynx"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm firefox tor lynx
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Información del Sistema"
-echo "neofetch htop glances net-tools iproute2 curl"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm neofetch htop glances net-tools iproute2 curl
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Escaneo de Red"
-echo "nmap"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm nmap
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Comprimir Archivos"
-echo "p7zip"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm p7zip
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Encriptación de Datos"
-echo "gpg"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm gpg
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Copias de Seguridad"
-echo "rsync duplicity"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm rsync duplicity
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Conversor de Imágenes a ASCII"
-echo "jp2a"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm jp2a
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Salvapantallas"
-echo "cmatrix"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm cmatrix
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Seguridad"
-echo "fail2ban iptables openssh snort"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm fail2ban iptables openssh snort
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Correo Electrónico"
-echo "mutt"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm mutt
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Gestor de Contraseñas"
-echo "pass"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm pass
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Juegos"
-echo "gnuchess frotz angband cataclysm-dda nethack"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm gnuchess frotz angband cataclysm-dda nethack
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-figlet "Misceláneo"
-echo "lolcat tree"
-echo "y / n"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm lolcat tree
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
+# Continuar con el resto de las secciones de la misma manera...
