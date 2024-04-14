408503c187e4e62d59baab9d510d3e798cd8e0a8
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-04-14 21:23:12 +0200

    Update 1st_run_without_sudo.sh

--- 1st_run_without_sudo.sh
+++ 1st_run_without_sudo.sh
@@ -24,10 +24,10 @@ fi
 
 clear
 figlet "¿AUR Games? [y/n]"
-cowsay -f /usr/share/cows/dragon.cow "ri-li 2048-cli tetris moon-buggy"
+cowsay -f /usr/share/cows/dragon.cow "ri-li 2048-cli tetris"
 read respuesta
 if [ "$respuesta" = "y" ]; then
-  yay -S --noconfirm ri-li 2048-cli tetris moon-buggy
+  yay -S --noconfirm ri-li 2048-cli tetris
 elif [ "$respuesta" = "n" ]; then
     echo "Saltando al siguiente apartado..."
 fi
