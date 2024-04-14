26727c3bc12de52db62b94e22a007a595426c531
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-04-14 20:48:40 +0200

    pequeños cambios

--- 1st_run_without_sudo.sh
+++ 1st_run_without_sudo.sh
@@ -5,13 +5,8 @@ mkdir aur
 cd aur
 
 git clone https://aur.archlinux.org/yay.git
-git clone https://aur.archlinux.org/lightdm-webkit2-greeter.git
 
 cd yay
-
-makepkg -si
-
-cd ../lightdm-webkit2-greeter
 
 makepkg -si
 
