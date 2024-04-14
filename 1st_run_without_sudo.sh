035fe5da13a18aef96efc93a6537f880f43eb68e
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-04-14 18:49:28 +0200

    Borrado instalaciones yay

--- 1st_run_without_sudo.sh
+++ 1st_run_without_sudo.sh
@@ -6,6 +6,7 @@ cd aur
 
 git clone https://aur.archlinux.org/yay.git
 git clone https://aur.archlinux.org/lightdm-webkit2-greeter.git
+
 cd yay
 
 makepkg -si
@@ -15,32 +16,3 @@ cd ../lightdm-webkit2-greeter
 makepkg -si
 
 cd ../
-
-clear
-figlet "¿Theme for lightdm?"
-cowsay -f /usr/share/cows/dragon.cow "Aether"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-
-	yay -S light-webkit-theme-aether
-
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-
-clear
-figlet "¿AUR Games?"
-cowsay -f /usr/share/cows/dragon.cow "moon-buggy ; ri-li ; 2048-cli ; tetris"
-read respuesta
-if [ "$respuesta" = "y" ]; then
-
-	yay -S ri-li 2048-cli tetris moon-buggy --noconfirm
-
-elif [ "$respuesta" = "n" ]; then
-    echo "Saltando al siguiente apartado..."
-fi
-
-
-
-
