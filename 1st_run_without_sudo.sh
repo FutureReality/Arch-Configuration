cf59a4a9bff4eb99b1c05981a744e7142f802d61
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-04-14 20:53:53 +0200

    Update 1st_run_without_sudo.sh

--- 1st_run_without_sudo.sh
+++ 1st_run_without_sudo.sh
@@ -12,5 +12,23 @@ makepkg -si
 
 cd ../
 
-yay -S --noconfirm lightdm-webkit2-greeter lightdm-webkit-theme-aether 
-yay -S --noconfirm ri-li 2048-cli tetris moon-buggy
+clear
+figlet "¿AUR Graphic Environment? [y/n]"
+cowsay -f /usr/share/cows/dragon.cow "lightdm-webkit2-greeter lightdm-webkit-theme-aether"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+  yay -S --noconfirm lightdm-webkit2-greeter lightdm-webkit-theme-aether 
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
+clear
+figlet "¿AUR Games? [y/n]"
+cowsay -f /usr/share/cows/dragon.cow "ri-li 2048-cli tetris moon-buggy"
+read respuesta
+if [ "$respuesta" = "y" ]; then
+  yay -S --noconfirm ri-li 2048-cli tetris moon-buggy
+elif [ "$respuesta" = "n" ]; then
+    echo "Saltando al siguiente apartado..."
+fi
+
