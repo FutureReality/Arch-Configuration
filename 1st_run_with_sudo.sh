43f36422b670b426d498500011f8ddabfab0bb17
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-04-15 13:09:56 +0200

    Update 1st_run_with_sudo.sh

--- 1st_run_with_sudo.sh
+++ 1st_run_with_sudo.sh
@@ -4,10 +4,10 @@ pacman -Sy figlet cowsay
 
 clear
 figlet "¿Install All? [y/n]"
-cowsay -f /usr/share/cows/dragon.cow "lightdm light-gtk-greeter qtile picom nitrogen arandr alacritty kitty nano neovim git subversion mc ranger fzf firefox tor lynx neofetch htop glances net-tools iproute2 curl btop usbutils pciutils wireshark-cli nmap tcpdump strace iftop iputils net-tools traceroute p7zip tar zip unzip gzip bzip2 gnupg rsync duplicity jp2a cmatrix fail2ban iptables openssh mutt pass gnuchess frotz angband cataclysm-dda nethack bsd-games lolcat tree bc base-devel"
+cowsay -f /usr/share/cows/dragon.cow "lightdm light-gtk-greeter qtile picom nitrogen arandr alacritty kitty nano neovim git subversion mc ranger fzf firefox tor lynx neofetch htop glances net-tools iproute2 curl btop usbutils pciutils wireshark-cli nmap tcpdump strace iftop iputils net-tools traceroute p7zip tar zip unzip gzip bzip2 gnupg rsync duplicity jp2a cmatrix fail2ban iptables openssh mutt pass gnuchess frotz angband cataclysm-dda nethack bsd-games lolcat tree bc base-devel nerd-fonts"
 read instalar_todo
 if [ "$instalar_todo" = "y" ]; then
-    pacman -Sy lightdm light-gtk-greeter qtile picom nitrogen arandr alacritty kitty nano neovim git subversion mc ranger fzf firefox tor lynx neofetch htop glances net-tools iproute2 curl btop usbutils pciutils wireshark-cli nmap tcpdump strace iftop iputils net-tools traceroute p7zip tar zip unzip gzip bzip2 gnupg rsync duplicity jp2a cmatrix fail2ban iptables openssh mutt pass gnuchess frotz angband cataclysm-dda nethack bsd-games lolcat tree bc base-devel
+    pacman -Sy lightdm light-gtk-greeter qtile picom nitrogen arandr alacritty kitty nano neovim git subversion mc ranger fzf firefox tor lynx neofetch htop glances net-tools iproute2 curl btop usbutils pciutils wireshark-cli nmap tcpdump strace iftop iputils net-tools traceroute p7zip tar zip unzip gzip bzip2 gnupg rsync duplicity jp2a cmatrix fail2ban iptables openssh mutt pass gnuchess frotz angband cataclysm-dda nethack bsd-games lolcat tree bc base-devel nerd-fonts
     exit 0
 elif [ "$instalar_todo" = "n" ]; then
     echo "Loading Stuff"
@@ -146,13 +146,10 @@ fi
 
 clear
 figlet "¿Misceláneo? [y/n]"
-cowsay -f /usr/share/cows/dragon.cow "lolcat tree bc cmatrix jp2a base-devel"
+cowsay -f /usr/share/cows/dragon.cow "lolcat tree bc cmatrix jp2a base-devel nerd-fonts"
 read respuesta
 if [ "$respuesta" = "y" ]; then
-    pacman -Sy --noconfirm lolcat tree
+    pacman -Sy --noconfirm lolcat tree nerd-fonts
 elif [ "$respuesta" = "n" ]; then
     echo "Saltando al siguiente apartado..."
 fi
-
-mkdir ~/.config/kitty
-mkdir ~/.config/qtile
