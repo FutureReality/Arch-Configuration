8c1c52b5f5a61ea70c9e5266e0949f0efc182a3b
FutureReality <115637305+FutureReality@users.noreply.github.com>
2024-04-15 13:11:18 +0200

    Update 3rt_run_with_sudo.sh

--- 3rt_run_with_sudo.sh
+++ 3rt_run_with_sudo.sh
@@ -4,6 +4,6 @@ rm ~/.config/qtile/config.py
 rm ~/.config/kitty/kitty.conf
 
 cp ./config.py ~/.config/qtile/
-cp ./kitty.conf ~/.config/kitty/
+cp ./alacritty.toml ~/.config/alacritty/
 
 reboot now
